c6bfc1cdffd54c91b66011364f66322dab0b2a13
Howard Ho <howard036060006@gmail.com>
2017-09-04 23:31:15 +0800

    modify dist package.json

--- dist/server.js
+++ dist/server.js
@@ -4,10 +4,8 @@ var serveStatic = require('serve-static');
 var history = require('connect-history-api-fallback')
 
 var app = express();
-app.use(history({
-  verbose: true
-}))
-app.use(express.static(__dirname);
+app.use(history())
+app.use(serveStatic(__dirname));
 var port = process.env.PORT || 5000;
 
 app.listen(port);
