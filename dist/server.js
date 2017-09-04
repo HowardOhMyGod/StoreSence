dec5ae7d570a3f18d5f45bff723c15328f5d8466
Howard Ho <howard036060006@gmail.com>
2017-09-04 23:27:36 +0800

    add verbose

--- dist/server.js
+++ dist/server.js
@@ -4,8 +4,10 @@ var serveStatic = require('serve-static');
 var history = require('connect-history-api-fallback')
 
 var app = express();
-app.use(history())
-app.use(serveStatic(__dirname));
+app.use(history({
+  verbose: true
+}))
+app.use(express.static(__dirname);
 var port = process.env.PORT || 5000;
 
 app.listen(port);
