8060ebd909dc1d40833bbcade65587d8b13ede71
Howard Ho <howard036060006@gmail.com>
2017-09-04 23:47:28 +0800

    set history

--- dist/server.js
+++ dist/server.js
@@ -11,7 +11,7 @@ app.use(history())
 app.use(staticFileMiddleware)
 // ^ `app.use(staticFileMiddleware)` is included twice as per https://github.com/bripkens/connect-history-api-fallback/blob/master/examples/static-files-and-index-rewrite/README.md#configuring-the-middleware
 
-app.get('/', function (req, res) {
+app.get('*', function (req, res) {
   res.render(path.join(__dirname + '/index.html'))
 })
 
